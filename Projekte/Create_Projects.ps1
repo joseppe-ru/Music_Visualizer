9c9581058edf920661925c0192297f336295fb4a
Russe420 <joauswvonr@web.de>
2023-10-05 14:08:24 +0200

    Scripte zum automatischen installieren der Module, also alles das, was nicht mit im Git liegt

---
+++ Projekte/Create_Projects.ps1
@@ -0,0 +1,13 @@
+﻿$Files = Get-ChildItem -File
+$Folders = Get-ChildItem -Exclude $Files 
+$script_endung = ".\*ps1"
+#Schleife sucht shell-scripte aus jedem Projekt herraus
+foreach ($Project in $Folders){
+    $Project
+    cd $Project
+    $script = Get-ChildItem -file -Name *.ps1 
+    #[Diagnostics.Process]::Start("powershell.exe",$Files)
+    & .\$script
+    cd ..
+}
+
